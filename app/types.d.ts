acd926d3ddf7cd4570b477e30c9dc6a5d17ebc50
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-08-16 18:26:14 -0300

    initial

---
+++ app/types.d.ts
@@ -0,0 +1,27 @@
+export interface RegisterForm { 
+    username: string
+    email: string
+    password: string
+}
+
+export interface User extends Omit<RegisterForm, 'password'> {}  // EMAIL Y USERNAME
+export interface LoginForm extends Omit<RegisterForm, 'username'> {} //  EMAIL Y PASSWORD
+
+export interface AppState {
+    user: User | null;
+    movies: Array<MovieObject>;
+    allMovies: Array<MovieObject> | [];
+    MyList: Array<MovieObject> | [];
+  }
+
+export interface MovieObject { 
+    background: string
+    date: string
+    gender: string
+    id: string
+    idi: number
+    image: string,
+    language: string
+    overview: string
+    title: string
+}
