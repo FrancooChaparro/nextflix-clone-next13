d4c866f6eb458db31f93f3e7b9d5b38edcf5ae28
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-08-17 15:48:39 -0300

    Navbar-done

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -47,24 +47,44 @@ export const Navbar = () => {
       <div className={styles.containerLogo}>
         <img src="./images/Netflix_Logo.png" alt="LogoNetflix" />
       </div>
-      <div className={styles.containerInfo}>
-       
-          <span className={styles.span1}>Home</span>
-        
+      <div className={styles.containerInfo}>   
+          <span className={styles.span1}>Home</span>  
           <span className={styles.span1}>Series</span>
-      
           <span className={styles.span1}>Films</span>
-       
           <span className={styles.span1}>New & Popular</span>
-        
           <span className={styles.span1}>My List</span>
-      
-          <span className={styles.span1}>Premium Netflix</span>
-    
+          <span className={styles.span1}>Premium Netflix</span> 
       </div>
 
+
+      <div className={styles.containerBrowse}>
+          <span
+            className={styles.btnBrowse}
+            onClick={() => setShowBrowse(!showBrowse)}
+          >
+            Menu
+          </span>
+              <BsChevronDown  className={styles.btnBrowse}/>
+
+
+        {showBrowse && (
+            <div
+            className={showBackground ? styles.Browse : styles.BrowseOpacity}
+            >
+              <p>Home</p>
+              <p>Series</p>
+              <p>Films</p> 
+              <p>New & Popular</p>
+              <p>My List</p>
+              <p>Premium Netflix</p>
+          </div>
+        )}
+        </div>
       </div>
+
+
       <div className={styles.containerUser}>
+        <div className={styles.containerUser1}>
             <BiSearch  />
             <IoNotificationsOutline  />
           <div className={styles.userDiv1}>
@@ -76,8 +96,26 @@ export const Navbar = () => {
               />
             </div>
           </div>
+          </div>
+          {showMenu && 
+          <div className={styles.showMenu}>
+                
+        <div className={styles.containerAvatar}>
+            <div className={styles.imagen}>
+                <img src="./images/Netflix-avatar.png" alt="Avatar" />
+            </div>
+            <div className={styles.username}>
+                <span>Franco Chaparro</span>
+            </div>
+        </div>
+        
+        <div className={styles.containerLogout}><span className={styles.spans}>Sign out of Netflix</span></div>
+    </div>
+}
         </div>
 
+
+          
 
       </div>
 
