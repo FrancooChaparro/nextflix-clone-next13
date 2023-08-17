d08ae2b9a9e170b5c1cdea65b46668a974adf49b
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-08-17 17:44:33 -0300

    Banner-movie

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,33 +1,53 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
 import styles from "@/styles/Banner.module.css";
 import { IoMdAdd } from "react-icons/io";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import { MoviesData } from "@/models/banner";
+import { MovieObject } from "@/app/types";
 
 const Banner = () => {
+  const Movies = MoviesData;
+  const [num, setNum] = useState<number>(0);
+
+  useEffect(() => {
+    if (num === 19) {
+      setNum(0);
+    }
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (num === 13) {
+        setNum(0);
+      } else {
+        setNum((num) => num + 1);
+      }
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, [num]);
+
+  const poster: MovieObject = Movies[num];
   return (
     <div className={styles.ContainerBackground}>
-    <img src="./images/img1.jpg" alt="logo" className={styles.background} />
-    <div className={styles.containerData}>
-      <p className={styles.title}>LA LLAMADA</p>
-      <p className={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error in
-        consequatur labore itaque dolore, earum, corporis officiis animi,
-        veniam deserunt ab laudantium sed? Deserunt debitis temporibus, quas
-        facere dolorem vitae.
-      </p>
-      <div className={styles.containerBtn}>
-        <button className={styles.btnAdd}>
-          <IoMdAdd className={styles.icon} />
-          Add List
-        </button>
-        <button className={styles.btnInfo}>
-          <AiOutlineInfoCircle className={styles.icon} />
-          More Info
-        </button>
+      <img src={poster.background} alt="logo" className={styles.background} />
+      <div className={styles.containerData}>
+        <p className={styles.title}>{poster.title}</p>
+        <p className={styles.description}>{poster.overview}</p>
+        <div className={styles.containerBtn}>
+          <button className={styles.btnAdd}>
+            <IoMdAdd className={styles.icon} />
+            Add List
+          </button>
+          <button className={styles.btnInfo}>
+            <AiOutlineInfoCircle className={styles.icon} />
+            More Info
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Banner;
