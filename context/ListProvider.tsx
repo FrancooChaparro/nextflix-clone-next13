12892c2158bbe9c3b868a4b55d2bea9fa0668b24
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-08-29 17:28:19 -0300

    inital

--- context/ListProvider.tsx
+++ context/ListProvider.tsx
@@ -8,26 +8,14 @@ interface MyProviderProps {
 }
 
 const MyProvider: FC<MyProviderProps> = ({ children }) => {
-  const [session, setSession] = useState<boolean>(() => {
-    const storedsession = localStorage.getItem("session");
-    return storedsession ? JSON.parse(storedsession) : false;
+  const [session, setSession] = useState<boolean>(false);
+  const [user, setUser] = useState<RegisterForm>({
+    username: "",
+    email: "",
+    password: "",
   });
 
-  const [user, setUser] = useState<RegisterForm>(() => {
-    const storeduser = localStorage.getItem("user");
-    return storeduser
-      ? JSON.parse(storeduser)
-      : {
-          username: "",
-          email: "",
-          password: "",
-        };
-  });
-
-  const [MyList, setMyList] = useState<MovieObject[]>(() => {
-    const storedList = localStorage.getItem("myList");
-    return storedList ? JSON.parse(storedList) : [];
-  });
+  const [MyList, setMyList] = useState<MovieObject[]>([]);
 
   const AddMyList = (newValue: MovieObject) => {
     const existentMovie = MyList.find((e) => e.title === newValue.title);
@@ -35,8 +23,6 @@ const MyProvider: FC<MyProviderProps> = ({ children }) => {
 
     const updatedList = [...MyList, newValue];
     setMyList(updatedList);
-
-    localStorage.setItem("myList", JSON.stringify(updatedList));
   };
 
   const DeleteMyList = (newValue: MovieObject) => {
@@ -44,7 +30,6 @@ const MyProvider: FC<MyProviderProps> = ({ children }) => {
       (movie) => movie.title !== newValue.title
     );
     setMyList(updatedList);
-    localStorage.setItem("myList", JSON.stringify(updatedList));
   };
 
   const OutSign = () => {
@@ -54,21 +39,20 @@ const MyProvider: FC<MyProviderProps> = ({ children }) => {
       password: "",
     });
     setSession(false);
-    localStorage.setItem("session", JSON.stringify(false));
-    localStorage.removeItem("user");
   };
+
   const RegisterAction = (newValue: RegisterForm) => {
     setUser(newValue);
-    localStorage.setItem("user", JSON.stringify(newValue));
   };
+
   const LoginAction = (newValue: LoginForm) => {
     if (user.email === newValue.email && user.password === newValue.password) {
-      localStorage.setItem("session", JSON.stringify(true));
       return setSession(true);
     }
 
     return alert("Usuario o contraseña invalido");
   };
+
   const contextValue: MyContextType = {
     MyList,
     AddMyList,
