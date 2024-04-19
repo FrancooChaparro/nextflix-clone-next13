d1fc88cde9f82aea233dc1945b4de0d79e640587
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-19 17:24:37 -0300

    clone

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Navbar />
       <Banner />
-      {/* <ContainerLands /> */}
+      <ContainerLands />
       <br /><br /><br /> <br />
       <br /><br /><br />
       <Footer />
